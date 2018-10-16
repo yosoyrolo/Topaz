b0234914dac3c8d6e5ca0157e1ddd8836ac2a843
Esteban Devia <rolohd@gmail.com>
2018-10-16 15:55:14 -0400

    Added functions
    -updateGui: Actualiza la interfaz grafica.
    
    -createNewProject: Crea un nuevo proyecto con una pagina.

--- app/js/logic.js
+++ app/js/logic.js
@@ -1,1 +1,40 @@
-alert("Hola mundo");
+///////////////////////////////////////////////////////////////////////////Paneles
+
+var pagesPanel = document.getElementById("pagesPanel");
+
+///////////////////////////////////////////////////////////////////////////
+var project = {};
+
+function updateGui(){
+	pagesPanel.innerHTML = "";
+	for (var i = 0; i < project.pages.length; i++) {
+		var pageDiv = document.createElement("div");
+		pageDiv.className = "pageThumbnail";
+		console.log(project.pages[i].name)
+		pageDiv.innerText = project.pages[i].name;
+		pagesPanel.appendChild(pageDiv);
+	}
+
+	var AddPageButton = document.createElement("div");
+}
+
+
+function createNewProject(){
+	project = {
+		pages: [
+						{
+							name: "index",
+						content: {}
+					}
+				]
+		,
+
+		images: {
+		}
+	}
+	updateGui()
+	return project;
+
+}
+
+createNewProject()
